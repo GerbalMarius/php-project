29c479b40ab4ff7bf24f7c655498bbf554b8dfc4
GerbalMarius <mariukas.ambrazevicius@gmail.com>
2024-12-03 01:27:28 +0200

    register page fix.

--- www/src/register-page.php
+++ www/src/register-page.php
@@ -23,7 +23,7 @@ session_start();
         <h1 class="form-heading">Susikurkite paskyrą</h1> <img src="images/home-icon.svg" alt="home" class="smaller-img-top-left"> <a class="smaller-img-top-left" href="../src"></a>
         <form action="actions/register.php" method="post">
             <label for="name" class="form-label">Vardas:</label>
-            <input type="text" name="name" class="form-input" value="<?php echo htmlspecialchars($_SESSION['input_name']) ?? ""; ?>" maxlength="80"  lang="lt-LT">
+            <input type="text" name="name" class="form-input" value="<?php echo htmlspecialchars($_SESSION['input_name'] ?? ""); ?>" maxlength="80"  lang="lt-LT">
             <?php
             if (isset($_GET["name"])) {
                 echo "<span class = 'error'>*Vardas yra būtinas.</span>";
@@ -31,7 +31,7 @@ session_start();
             ?>
             <p></p>
             <label for="last-name" class="form-label">Pavardė:</label>
-            <input type="text" name="last-name" class="form-input" value="<?php echo htmlspecialchars($_SESSION['input_last_name']) ?? ""; ?>" maxlength="80">
+            <input type="text" name="last-name" class="form-input" value="<?php echo htmlspecialchars($_SESSION['input_last_name'] ?? ""); ?>" maxlength="80">
             <?php
             if (isset($_GET["last_name"])) {
                 echo "<span class = 'error'>*Pavardė yra būtina.</span>";
@@ -39,7 +39,7 @@ session_start();
             ?>
             <p></p>
             <label for="email" class="form-label">El.paštas:</label>
-            <input type="email" name="email" class="form-input" value="<?php echo htmlspecialchars($_SESSION['input_email']) ?? ""; ?>">
+            <input type="email" name="email" class="form-input" value="<?php echo htmlspecialchars($_SESSION['input_email'] ?? ""); ?>">
             <?php
             if (isset($_GET["email"])) {
                 echo "<span class = 'error'>*El.paštas yra būtinas.</span>";
@@ -47,7 +47,7 @@ session_start();
             ?>
             <p></p>
             <label for="password" class="form-label">Slaptažodis:</label>
-            <input type="password" name="password" class="form-input" maxlength="80" value="<?php echo htmlspecialchars($_SESSION['input_password']) ?? ""; ?>">
+            <input type="password" name="password" class="form-input" maxlength="80" value="<?php echo htmlspecialchars($_SESSION['input_password'] ?? ""); ?>">
             <?php
             if (isset($_GET["password"])) {
                 echo "<span class = 'error'>*Slaptažodis yra būtinas.</span>";
@@ -55,7 +55,7 @@ session_start();
             ?>
             <p></p>
             <label for="repeated-passwd" class="form-label">Pakartokite slaptažodį:</label>
-            <input type="password" name="repeated-passwd" value="<?php echo htmlspecialchars($_SESSION['input_repeat']) ?? ""; ?>" class="form-input" maxlength="80">
+            <input type="password" name="repeated-passwd" value="<?php echo htmlspecialchars($_SESSION['input_repeat'] ?? ""); ?>" class="form-input" maxlength="80">
             <?php
             if (isset($_GET["repeated_passwd"])) {
                 echo "<span class = 'error'>*Slaptažodis nesutampa.</span>";
@@ -63,7 +63,7 @@ session_start();
             ?>
             <p></p>
             <label for="birthdate" class="form-label">Pasirinkite gimimo datą:</label>
-            <input type="date" name="birthdate" min="1920-01-01" max="<?php echo date('Y-m-d'); ?>" class="form-input" lang="lt-LT" value="<?php echo htmlspecialchars($_SESSION['input_birthdate']) ?? ""; ?>">
+            <input type="date" name="birthdate" min="1920-01-01" max="<?php echo date('Y-m-d'); ?>" class="form-input" lang="lt-LT" value="<?php echo htmlspecialchars($_SESSION['input_birthdate'] ?? ""); ?>">
             <?php
             if (isset($_GET["birthdate"])) {
                 echo "<span class = 'error'>*Gim. data yra būtina.</span>";
@@ -71,7 +71,7 @@ session_start();
             ?>
             <p></p>
             <label for="telephone-number" class="form-label">Telefono nr.:</label>
-            <input type="text" name="telephone-number" class="form-input" value="<?php echo htmlspecialchars($_SESSION['input_telephone_number']) ?? ""; ?>">
+            <input type="text" name="telephone-number" class="form-input" value="<?php echo htmlspecialchars($_SESSION['input_telephone_number'] ?? ""); ?>">
             <?php
             if (isset($_GET["telephone_number"])) {
                 echo "<span class = 'error'>*Telefono nr. yra būtinas.</span>";
